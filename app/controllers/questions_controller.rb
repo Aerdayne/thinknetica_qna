f3d70cf9ce6c7cceceb95c5eef9ccd2336682c54
Aerdayne <daenskus@gmail.com>
2019-11-19 01:09:48 +0300

    Answer controller & specs
    - Answer controller basic actions
    - Answer basic actions specs
    - Decent_exposure refactoring

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,6 @@
 class QuestionsController < ApplicationController
-  def index
-    @questions = Question.all
-  end
-
-  def show; end
-
-  def new; end
-
-  def edit; end
+  expose :question
+  expose :questions, -> { Question.all }
 
   def create
     @question = Question.new(question_params)
@@ -21,7 +14,7 @@ class QuestionsController < ApplicationController
 
   def update
     if question.update(question_params)
-      redirect_to @question
+      redirect_to question_path(question)
     else
       render :edit
     end
@@ -31,13 +24,8 @@ class QuestionsController < ApplicationController
     question.destroy
     redirect_to questions_path
   end
+
   private
-
-  def question
-    @question ||= params[:id] ? Question.find(params[:id]) : Question.new
-  end
-
-  helper_method :question
 
   def question_params
     params.require(:question).permit(:body, :title)
