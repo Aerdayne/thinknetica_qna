87664cdd53740d322c5a87ce00d5ac8dcca11984
Aerdayne <daenskus@gmail.com>
2019-11-18 13:23:51 +0300

    Fixes
    - Answers are now destroyed instead of nullified on Question deletion
    - Answer model spec fix
    - Question model spec fix

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,6 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
   it { should validate_presence_of :body }
-  it { should have_db_column :body }
   it { should belong_to :question }
 end
