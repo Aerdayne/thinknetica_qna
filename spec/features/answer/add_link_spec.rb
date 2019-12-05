192b6519b501333392611d3b89114a2f4dae6452
Aerdayne <daenskus@gmail.com>
2019-12-05 21:20:42 +0300

    User can attach several links to an answer

--- spec/features/answer/add_link_spec.rb
+++ spec/features/answer/add_link_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature 'User can add links to an answer', %q{
   In order to provide additional info to my answer
-  As an question's author
+  As a question's author
   I'd like to be able to add links
 } do
 
@@ -10,17 +10,31 @@ feature 'User can add links to an answer', %q{
   given!(:question) { create(:question, user: user) }
   given(:gist_url) {'https://gist.github.com/jacksonfdam/3000275'}
 
-  scenario 'User adds a link while giving an answer', js: true do
+  scenario 'User adds several links while giving an answer', js: true do
     sign_in(user)
     visit question_path(question)
 
-    fill_in 'Body', with: 'My answer'
-    fill_in 'Link name', with: 'My gist'
-    fill_in 'Url', with: gist_url
-    click_on 'Answer'
+    within '.answer-new' do
+      click_on 'Add link'
+
+      fill_in 'Body', with: 'My answer'
+
+      within all('.nested-fields')[0] do
+        fill_in 'Link name', with: 'My gist'
+        fill_in 'Url', with: gist_url
+      end
+
+      within all('.nested-fields')[1] do
+        fill_in 'Link name', with: 'Another gist'
+        fill_in 'Url', with: gist_url
+      end
+
+      click_on 'Answer'
+    end
 
     within '.answers' do
       expect(page).to have_link 'My gist', href: gist_url
+      expect(page).to have_link 'Another gist', href: gist_url
     end
   end
 
