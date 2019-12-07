ba777e8743074e2249137538870a48c269a4cd54
Aerdayne <daenskus@gmail.com>
2019-12-07 12:34:44 +0300

    LinksController#destroy fix

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,10 +1,12 @@
 class LinksController < ApplicationController
   before_action :authenticate_user!
 
+  expose :link
+
   def destroy
-    @link = Link.find(params[:id])
-    if current_user.author_of?(@link.linkable)
-      @link.destroy
+    @resource = link.linkable
+    if current_user.author_of?(@resource)
+      link.destroy
     end
   end
 
